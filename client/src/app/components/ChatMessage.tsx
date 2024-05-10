8a17d10696f983dee120b1e2caa9ce15c3239c98
Kazuki Takahashi <narita30830613@gmail.com>
2024-05-10 10:21:57 +0900

    prittierで綺麗にした

--- client/src/app/components/ChatMessage.tsx
+++ client/src/app/components/ChatMessage.tsx
@@ -1,7 +1,7 @@
-'use client'
-import React from 'react'
-import { useRecoilState } from 'recoil';
-import { chatLogState } from '../../../states/chatLogState';
+"use client";
+import React from "react";
+import { useRecoilState } from "recoil";
+import { chatLogState } from "../../../states/chatLogState";
 
 type MessageType = {
   id: number;
@@ -10,28 +10,35 @@ type MessageType = {
 };
 
 const ChatMessage = () => {
-  const [chatLog, setChatLog] = useRecoilState(chatLogState)
+  const [chatLog, setChatLog] = useRecoilState(chatLogState);
   return (
     <>
-      {chatLog.map((message:MessageType) => {
+      {chatLog.map((message: MessageType) => {
         return (
-          <div 
-	    key={message.id} 
-	    className={`flex items-end ${message.sender === 'user' ? 'justify-end' : ''}`}>
-            {message.sender === 'other' && (
+          <div
+            key={message.id}
+            className={`flex items-end ${
+              message.sender === "user" ? "justify-end" : ""
+            }`}
+          >
+            {message.sender === "other" && (
               <div className="flex-shrink-0 mr-2">
-                <div className="h-8 w-8 bg-gray-300 rounded-full" /> {/* アイコンの代わり */}
+                <div className="h-8 w-8 bg-gray-300 rounded-full" />{" "}
+                {/* アイコンの代わり */}
               </div>
             )}
-            <div 
-	     className={`rounded p-2 ${message.sender === 'user' ? 'bg-blue-200' : 'bg-gray-500'}`}>
+            <div
+              className={`rounded p-2 ${
+                message.sender === "user" ? "bg-blue-200" : "bg-gray-500"
+              }`}
+            >
               <p className="text-sm">{message.content}</p>
             </div>
           </div>
-        )
+        );
       })}
     </>
-  )
-}
+  );
+};
 
-export default ChatMessage
+export default ChatMessage;
