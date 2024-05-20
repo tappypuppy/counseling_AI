89a0381b36cb6dfa1f6241bf8427cd14f2121509
Kazuki Takahashi <narita30830613@gmail.com>
2024-05-20 13:46:47 +0900

    folder name 変更

--- client/src/app/newchat/page.tsx
+++ client/src/app/newchat/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import ChatPage from "@/components/ChatUI/ChatPage/ChatPage";
+import ChatPage from "@/components/NewChatUI/ChatPage/ChatPage";
 import { SessionProvider } from "next-auth/react";
 import { RecoilRoot } from "recoil";
 
