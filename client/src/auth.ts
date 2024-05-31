7ae225d02c40b79d0ebc465bd49d7c5b6c22a2ed
Kazuki Takahashi <narita30830613@gmail.com>
2024-05-31 19:02:15 +0900

    signinのstripe顧客作成を削除

--- client/src/auth.ts
+++ client/src/auth.ts
@@ -18,37 +18,6 @@ export const config: NextAuthConfig = {
   }),
   basePath: "/api/auth",
   callbacks: {
-    async signIn({ user, account, profile }) {
-      // TODO: 顧客情報をcookie or session に保存
-      const response = await fetch(
-        `${process.env.NEXTAUTH_URL}/api/create_stripe_customer`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            userId: user?.id,
-            userName: user?.name,
-            userEmail: user?.email,
-          }),
-        }
-      );
-
-      console.log("response: ", response);
-
-      // // サインイン時に Stripe 顧客を作成
-      // if (!user?.id) {
-      //   throw new Error("User ID is undefined");
-      // }
-      // const stripeCustomer = await createStripeCustomer(
-      //   user?.id ?? "",
-      //   user?.name ?? "",
-      //   user?.email ?? ""
-      // );
-      // console.log("stripeCustomer: ", stripeCustomer);
-      return true;
-    },
     authorized({ request, auth }) {
       try {
         const { pathname } = request.nextUrl;
