d2a671ac71aa5b8be5672aca9971b501341c8b03
Kazuki Takahashi <narita30830613@gmail.com>
2024-05-31 19:01:21 +0900

    add comment

--- client/src/components/Payment/Payment.tsx
+++ client/src/components/Payment/Payment.tsx
@@ -7,6 +7,8 @@ export const Payment = () => {
   const { push } = useRouter();
 
   async function handleClick() {
+
+    // customer idの取得, なければ作成
     const data = await fetch("/api/create_stripe_customer", {
         method: "POST",
         headers: {
@@ -19,19 +21,23 @@ export const Payment = () => {
         }),
         }).then((data) => data.json());
 
-    const costumerId = await data.customerId;
-    
-    console.log('DATA: ', costumerId);
+    const costumerId = data.customerId;
+
+    // 支払いセッションの作成
     const response = await fetch("/api/checkout_sessions", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        customer_id: costumerId, // "cus_Q8lKDUCBhbp5aU", // ここに顧客IDを値に設定しましょう
+        customer_id: costumerId,
+
+        // envに格納した方がいいかも
         price_id: "price_1PITZBHfFYNX5yiouTo4Yy8w", // ここに商品IDを値に設定しましょう
       }),
     }).then((data) => data.json());
+
+    // 支払い画面へ遷移
     push(response.checkout_url);
   }
 
